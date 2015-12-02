177512987ee4a4c14ec61176ba8e9509555a59b5
Elizabwth <lizabwth@gmail.com>
2015-12-01 21:18:52 -0800

    initial commit

---
+++ run.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
+import src
+from src import utils
+
+app = Flask(__name__)
+app.secret_key = 'KPwdT3zJ2QUge3Xc'
+#app.debug = True
+
+utils.bundle_assets(app)
+
+# Root (Home)
+@app.route('/')
+def index():
+	return render_template('pages/index.html')
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+var load_background = function() {
+	var bg_picture = '/static/res/img/bg/bgbw.jpg';
+	$.ajax({ 
+		url: bg_picture, 
+		cache: true,
+		processData: false,
+	}).always(function() {
+		$('#background')
+			.css('background-image', "url("+bg_picture+")")
+			.fadeTo(1500, 1);
+		$('#background_gradient').fadeTo(2500, 0);
+	});
+}
+
+$(window).load(function() {
+	load_background();
+});
+
+$(document).ready(function() {
+	$('#content').fadeTo(500, 1);
+});
