8ac4dec9728b6d7899bd38d32cc0b1346781c25d
Elizabwth <lizabwth@gmail.com>
2016-03-26 13:03:26 -0700

    it's all about STYLE

--- static/js/main.js
+++ static/js/main.js
@@ -12,19 +12,8 @@ var load_background = function() {
 	});
 }
 
-var set_content_height = function() {
-	var window_height = $('body').height();
-	var content = $('.content');
-
-	var nav_height = $('.nav').height();
-	var footer_height = $('.footer').height();
-	var min_height = window_height - nav_height - footer_height;
-	content.css({'min-height': min_height});
-}
-
 $(window).load(function() {
 	load_background();
-	set_content_height();
 });
 
 $(document).ready(function() {
