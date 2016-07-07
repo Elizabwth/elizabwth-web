64ff0c3fcf7be037ded2d82d70684b9115f4e85e
Elizabwth <lizabwth@gmail.com>
2016-07-06 19:26:04 -0700

    faster blur technique and res

--- static/js/main.js
+++ static/js/main.js
@@ -3,37 +3,30 @@ $(window).load(function() {
 	//$('#background').fadeTo(1000, 1);
 	$('#background_gradient').fadeTo(2000, 0);
 
-	var bg = document.getElementById('background');
-	var bg_width = bg.naturalWidth;
-	var bg_height = bg.naturalHeight;
+	var bg_img = document.getElementById("bg_img");
+	var canvas = $("#background")[0];
+	var context = canvas.getContext("2d");
+
+	var resize_and_draw_bg = function(canvas_id, image) {
+		$(canvas_id).attr('width', $('html').width()+"px");
+		$(canvas_id).attr('height', $('html').height()+"px");
+
+		canvas = $(canvas_id)[0];
+		context = canvas.getContext("2d");
+		drawImageProp(context, image, 0, 0, canvas.width, canvas.height);
+	}
 
 	var apply_blur = function() {
-		var doc_width = $(document).width();
-		var doc_height = $(document).height();
-
-		var x_dif = bg_width - doc_width;
-		var y_dif = bg_height - doc_height;
+		resize_and_draw_bg('#background', bg_img)
 
 		$('.blur').each(function(index) {
-			var id_name = "blur_"+index;
 			var p = $(this).offset();
-			var x = p.left + (x_dif/2);
-			var y = p.top + (y_dif/2);
+			var x = p.left;
+			var y = p.top;
 			var w = $(this).outerWidth();
 			var h = $(this).outerHeight();
 
-			var canvas = $("#"+id_name);
-			if (canvas.length == 0) {
-				var canvas = $('<canvas id="'+id_name+'" style="position:absolute;z-index:-10;top:0;left:0;"/>');
-				$(this).prepend(canvas);
-			}
-			canvas[0].width = w;
-			canvas[0].height = h;
-
-			var canvas = canvas[0];
-			var context = canvas.getContext("2d");
-			context.drawImage(bg, x, y, w, h, 0, 0, w, h);
-			stackBoxBlurCanvasRGB(id_name, 0, 0, w, h, 10, 1);
+			StackBlur.canvasRGB(canvas, x, y, w, h, 10);
 		});
 	}
 
